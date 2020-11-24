4cb754d5ef78b115c5ae6acfa52ce5501725fabe
The Peeps191 <74725787+ThePeeps191@users.noreply.github.com>
2020-11-23 18:27:13 -0700

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,15 @@
+import random
+
+def guessing_game():
+	num = random.randint(1, 101)
+	while True:
+		i = int(input("Guess a number between one and one hundred:\n"))
+		if i == num:
+			print("You guessed correctly!")
+			break
+		elif i > num:
+			print("You guessed too high. Try again.\n")
+		else:
+			print("You guessed too low. Try again.\n")
+
+guessing_game()
